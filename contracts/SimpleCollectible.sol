8d8597e6c1cc9a37737ae9cc41524744aa6d22f7
Farzam Mohammadi <72108716+FarzamMohammadi@users.noreply.github.com>
2022-02-28 17:36:57 -0500

    Advanced collectible contract complete

--- contracts/SimpleCollectible.sol
+++ contracts/SimpleCollectible.sol
@@ -4,7 +4,6 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
-import "@openzeppelin/contracts/utils/Counters.sol";
 
 contract SimpleCollectible is ERC721URIStorage {
     uint256 public tokenCounter;
